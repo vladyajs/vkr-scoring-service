253e74839b7eb3a8003530c164b1e995daadec1c
Владислав Андреевич Шипай <shipay@atb.su>
2025-03-11 20:50:12 +0300

    initial commit

---
+++ src/main/java/com/vkr/vkr_scoring_service/VkrScoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vkr.vkr_scoring_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkrScoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VkrScoringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vkr/vkr_scoring_service/VkrScoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vkr.vkr_scoring_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkrScoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
